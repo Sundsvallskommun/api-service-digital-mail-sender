676e38a980a405ac23a3be5a08cef1f7ac54ee5b
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2023-12-19 16:35:36 +0100

    WIP

--- src/test/java/se/sundsvall/digitalmail/api/model/validation/HtmlValidatorTests.java
+++ src/test/java/se/sundsvall/digitalmail/api/model/validation/HtmlValidatorTests.java
@@ -1,38 +1,46 @@
 package se.sundsvall.digitalmail.api.model.validation;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import jakarta.validation.ValidationException;
 import se.sundsvall.dept44.test.annotation.resource.Load;
 import se.sundsvall.dept44.test.extension.ResourceLoaderExtension;
 
 @ExtendWith(ResourceLoaderExtension.class)
 class HtmlValidatorTests {
 
-    private final HtmlValidator validator = new HtmlValidator();
+	private final HtmlValidator validator = new HtmlValidator();
 
-    @Test
-    void validValue(@Load("valid-html.base64") final String html) {
-        assertThat(validator.validate(html)).isTrue();
-    }
+	@Test
+	void validValue(@Load("valid-html.base64") final String html) {
+		assertThat(validator.validate(html)).isTrue();
+	}
 
-    @Test
-    void invalidValue(@Load("invalid-html.base64") final String html) {
-        assertThat(validator.validate(html)).isFalse();
-    }
+	@Test
+	void invalidValue(@Load("invalid-html.base64") final String html) {
+		assertThat(validator.validate(html)).isFalse();
+	}
 
-    @ParameterizedTest(name = "[{index}] value=\"{0}\"")
-    @ValueSource(strings = {"", "   "})
-    void emptyHtml(final String value) {
-        assertThat(validator.validate(value)).isFalse();
-    }
+	@ParameterizedTest(name = "[{index}] value=\"{0}\"")
+	@ValueSource(strings = { "", "   " })
+	void emptyHtml(final String value) {
+		assertThat(validator.validate(value)).isFalse();
+	}
 
-    @Test
-    void nullValue() {
-        assertThat(validator.validate(null)).isFalse();
-    }
+	@Test
+	void nullValue() {
+		assertThat(validator.validate(null)).isFalse();
+	}
+
+	@Test
+	void throwsException() {
+		final var e = assertThrows(ValidationException.class, () -> validator.validate("x"));
+		assertThat(e.getMessage()).isEqualTo("Unable to validate HTML");
+	}
 }
