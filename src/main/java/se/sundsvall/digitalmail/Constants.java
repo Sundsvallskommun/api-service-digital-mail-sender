c68ddec2fa017ac6d498b6e8cf57455493c4cc2c
AndreSoderlund <andre.soderlund@b3.se>
2025-08-13 15:48:43 +0200

    Acted on sonar issues

--- src/main/java/se/sundsvall/digitalmail/Constants.java
+++ src/main/java/se/sundsvall/digitalmail/Constants.java
@@ -1,5 +1,8 @@
 package se.sundsvall.digitalmail;
 
-public class Constants {
+public final class Constants {
+
+	private Constants() {}
+
 	public static final String DEFAULT_SENDER_NAME = "Sundsvalls Kommun";
 }
--- src/test/java/se/sundsvall/digitalmail/integration/minameddelanden/DigitalMailDtoTest.java
+++ src/test/java/se/sundsvall/digitalmail/integration/minameddelanden/DigitalMailDtoTest.java
@@ -1,15 +1,28 @@
 package se.sundsvall.digitalmail.integration.minameddelanden;
 
+import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanEquals;
+import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanHashCode;
+import static com.google.code.beanmatchers.BeanMatchers.hasValidGettersAndSetters;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.allOf;
 import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
 
 import java.util.ArrayList;
+import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Test;
 import se.sundsvall.digitalmail.api.model.BodyInformation;
 import se.sundsvall.digitalmail.api.model.DigitalMailRequest;
 import se.sundsvall.digitalmail.api.model.SupportInfo;
 
 class DigitalMailDtoTest {
+
+	@Test
+	void testBean() {
+		MatcherAssert.assertThat(DigitalMailDto.class, allOf(
+			hasValidGettersAndSetters(),
+			hasValidBeanHashCode(),
+			hasValidBeanEquals()));
+	}
 
 	@Test
 	void testConstructor() {
